3e29cac021c87f7e895f32192712fda6ff06c1bd
David Maldonado <david.m.maldonado@gmail.com>
2013-12-18 13:11:15 -0500

    add submodules

--- init.el
+++ init.el
@@ -97,6 +97,13 @@
 (require 'haml-mode)
 (require 'slim-mode)
 
+;; markdown
+(autoload 'markdown-mode "markdown-mode"
+  "Major mode for editing Markdown files" t)
+(add-to-list 'auto-mode-alist '("\\.test\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 ;; autopair
 (require 'autopair)
 (autopair-global-mode)
