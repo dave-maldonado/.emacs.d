e59a543eec9ef0521d06a55e6f7131d3c630cec4
dave-maldonado <david.m.maldonado@gmail.com>
2014-08-12 07:21:52 -0400

    turned off electric indent for haskell

--- init.d/haskell-conf.el
+++ init.d/haskell-conf.el
@@ -6,3 +6,6 @@
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
+
+;; turn off electric indent for haskell
+(add-hook 'haskell-mode-hook (lambda () (electric-indent-local-mode -1)))
